c1c9274824875b3867034dfac61c1a15e4ce08aa
DevWithAbarna <abarnasri9977@gmail.com>
2024-12-26 13:29:38 +0400

    Add project files.

---
+++ CRUDInBlazor/Data/MyAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDInBlazor.Model;
+
+namespace CRUDInBlazor.Data
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext (DbContextOptions<MyAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDInBlazor.Model.Product> Product { get; set; } = default!;
+    }
+}
---
+++ CRUDInBlazor/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUDInBlazor.Model
+{
+    public class Product
+    {
+
+        public Guid id { get; set; }
+
+        public string name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Qty { get; set; }
+    }
+}
